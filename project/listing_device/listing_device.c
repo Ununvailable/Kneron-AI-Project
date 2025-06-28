0333a4e9c41d12eca07188ae4ea7218c0ccf009d
Lhieunguyen <nguyenhieuquynhdi@gmail.com>
2025-06-28 15:36:13 +0700

    Some updates for the SDK

---
+++ project/listing_device/listing_device.c
@@ -0,0 +1,21 @@
+/**
+ * @file        scan_devices.c
+ * @brief       example of scanning Kneron KL series device
+ * @version     0.1
+ * @date        2021-03-22
+ *
+ * @copyright   Copyright (c) 2021 Kneron Inc. All rights reserved.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "kp_core.h"
+
+int main() {
+    printf("My custom inference program\n");
+    // Insert SDK initialization / connection / inference logic
+    return 0;
+}
