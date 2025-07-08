f0ca5c6a8f27c3abec7b12baf7fa713a1a9ed4f3
Lhieunguyen <nguyenhieuquynhdi@gmail.com>
2025-07-08 12:06:56 +0800

    KL520 C++ SDK into a Flutter Android app via Dart FFI

---
+++ kneron_ai_project/lib/kl520_ffi.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+import 'dart:io';
+
+// C function signature: int kl520_run_inference(uint8_t* input_buf, int len);
+typedef Kl520RunInferenceC = Int32 Function(Pointer<Uint8>, Int32); // Native C
+typedef Kl520RunInferenceDart = int Function(Pointer<Uint8>, int);  // Dart
+typedef Kl520InitializeC = Int32 Function();
+typedef Kl520InitializeDart = int Function();
+
+final DynamicLibrary kl520Lib = Platform.isAndroid
+    ? DynamicLibrary.open('libkl520_infer.so')
+    : throw UnsupportedError('Only Android supported');
+
+final Kl520InitializeDart kl520Initialize = kl520Lib
+    .lookup<NativeFunction<Kl520InitializeC>>('kl520_initialize')
+    .asFunction<Kl520InitializeDart>();
+
+final Kl520RunInferenceDart kl520RunInference = kl520Lib
+    .lookup<NativeFunction<Kl520RunInferenceC>>('kl520_run_inference')
+    .asFunction<Kl520RunInferenceDart>();
