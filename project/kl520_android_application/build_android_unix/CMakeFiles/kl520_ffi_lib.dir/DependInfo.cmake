4c790c1eecf6f02b1284ddf8def1e5453dcdfa80
Ununvailable <leminhduc264994@gmail.com>
2025-07-30 14:41:08 +0700

    Minor changes

---
+++ project/kl520_android_application/build_android_unix/CMakeFiles/kl520_ffi_lib.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/Kneron-AI-Project/ex_common/helper_functions.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/ex_common/helper_functions.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/ex_common/helper_functions.c.o.d"
+  "F:/Kneron-AI-Project/ex_common/postprocess.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/ex_common/postprocess.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/ex_common/postprocess.c.o.d"
+  "F:/Kneron-AI-Project/src/kne_reader.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kne_reader.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kne_reader.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_core.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_core.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_core.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_core_wrapper.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_core_wrapper.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_core_wrapper.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_errstring.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_errstring.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_errstring.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_inference.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_inference.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_inference.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_set_key.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_set_key.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_set_key.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_update_flash.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_update_flash.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_update_flash.c.o.d"
+  "F:/Kneron-AI-Project/src/kp_usb.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_usb.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/kp_usb.c.o.d"
+  "F:/Kneron-AI-Project/src/model_descriptor_builder.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/model_descriptor_builder.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/model_descriptor_builder.c.o.d"
+  "F:/Kneron-AI-Project/src/model_descriptor_copier.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/model_descriptor_copier.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/model_descriptor_copier.c.o.d"
+  "F:/Kneron-AI-Project/src/model_reader_utils.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/model_reader_utils.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/model_reader_utils.c.o.d"
+  "F:/Kneron-AI-Project/src/nef_reader.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/nef_reader.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/nef_reader.c.o.d"
+  "F:/Kneron-AI-Project/src/setup_reader.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/setup_reader.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/setup_reader.c.o.d"
+  "F:/Kneron-AI-Project/src/utils.c" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/utils.c.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/F_/Kneron-AI-Project/src/utils.c.o.d"
+  "F:/Kneron-AI-Project/project/kl520_android_application/kl520_android_application.cpp" "CMakeFiles/kl520_ffi_lib.dir/kl520_android_application.cpp.o" "gcc" "CMakeFiles/kl520_ffi_lib.dir/kl520_android_application.cpp.o.d"
+  "" "libkl520_ffi_lib.so" "gcc" "CMakeFiles/kl520_ffi_lib.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/kl520_android_application/build_android_unix/CMakeFiles/kl520_ffi_lib.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for kl520_ffi_lib.
+# This may be replaced when dependencies are built.
